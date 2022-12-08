75c29959123f35d04081616d1ea025884640ab55
Matthew Kim <matthew.kim.733@my.csun.edu>
2022-12-08 14:37:09 -0800

    made css grid visible  based on slider

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 "use strict";
+
+//first we need to set the value to number of columns
+//make grid perfect square
+
 const gridContainer = document.querySelector(".grid-container");
 const range = document.getElementById("range");
 const label = document.getElementById("label");
-gridContainer.style.gridTemplateColumns = "repeat(2, 1fr)";
 
-for (let i = 0; i < 20; i++) {
-  const div = document.createElement("div");
-  div.textContent = "123";
-  gridContainer.insertAdjacentElement("afterbegin", div);
+function populateGrid(gridSize) {
+  const columns = gridSize / 2;
+  gridContainer.textContent = "";
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < gridSize; i++) {
+    const div = document.createElement("div");
+    div.textContent = '123'
+    div.classList.add("grid-item");
+    gridContainer.insertAdjacentElement("afterbegin", div);
+  }
 }
 
 range.addEventListener("click", function () {
   const value = range.value;
   label.textContent = `${range.value}`;
+  populateGrid(value);
 });
