61ffe92527a00c09aa5fe45c387cac10fcd0f086
Matthew Kim <matthew.kim.733@my.csun.edu>
2022-12-08 13:25:36 -0800

    made slider functional

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 const gridContainer = document.querySelector(".grid-container");
+const range = document.getElementById("range");
+const label = document.getElementById("label");
 gridContainer.style.gridTemplateColumns = "repeat(2, 1fr)";
 
 for (let i = 0; i < 20; i++) {
@@ -7,3 +9,8 @@ for (let i = 0; i < 20; i++) {
   div.textContent = "123";
   gridContainer.insertAdjacentElement("afterbegin", div);
 }
+
+range.addEventListener("click", function () {
+  const value = range.value;
+  label.textContent = `${range.value}`;
+});
