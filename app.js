f002564aecda820f8486ba16431379a499b468e0
Matthew Kim <matthew.kim.733@my.csun.edu>
2022-12-08 18:25:48 -0800

    styled UI

--- app.js
+++ app.js
@@ -2,18 +2,19 @@
 
 //first we need to set the value to number of columns
 //make grid perfect square
-
+window.addEventListener("load", function () {
+  populateGrid(1);
+});
 const gridContainer = document.querySelector(".grid-container");
 const range = document.getElementById("range");
 const label = document.getElementById("label");
 
 function populateGrid(gridSize) {
-  const columns = gridSize / 2;
   gridContainer.textContent = "";
   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   for (let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement("span");
+    const div = document.createElement("div");
     div.textContent = "";
     div.classList.add("grid-item");
     gridContainer.insertAdjacentElement("afterbegin", div);
@@ -25,13 +26,15 @@ function changeColor(e) {
 }
 
 range.addEventListener("input", function () {
-  label.textContent = `${range.value}`;
+  label.textContent = `${range.value}X${range.value}`;
 });
-range.addEventListener("click", function () {
+
+range.addEventListener("mouseup", function () {
   populateGrid(range.value);
 });
 
 gridContainer.addEventListener("mousedown", function (e) {
   if (!e.target.classList.contains("grid-item")) return;
+
   changeColor(e);
 });
